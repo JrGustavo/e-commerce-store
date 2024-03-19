6ada2e2eacdaf370daa308aa04ae18957f0befca
JrGustavo <gus@soypetal.com>
2024-03-19 16:43:12 -0500

    Update e-commerce-store

---
+++ app/components/footer.tsx
@@ -0,0 +1,17 @@
+
+const Footer = () => {
+    return (
+        <footer className="bg-white border-t">
+            <div className="mx-auto py-10">
+                <p className="text-center text-xs text-black">
+                    &copy; 2024 Petal S.A.S, Todos los derechos reservados
+
+                </p>
+
+            </div>
+
+        </footer>
+    )
+}
+
+export default Footer;
